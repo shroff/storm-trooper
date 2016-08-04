c68b55229853cea5b3eb22704b1822d910a2b6b1
Abhishek Shroff <ashroff6@gmail.com>
2016-08-03 23:48:53 -0700

    WIP

--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,35 @@
 #include "StripComponent.h"
 #include "CommandReader.h"
 
+#define CMD_TERMINATOR "x"
+#define NUM_COMPONENTS 1
+
 // Number of RGB LEDs in strand:
 const int STRIP_LED_COUNT = 32;
 const int STRIP_DATA_PIN  = 2;
 const int STRIP_CLOCK_PIN = 3;
-Component *stripComponent = new StripComponent(STRIP_LED_COUNT, STRIP_DATA_PIN, STRIP_CLOCK_PIN);
 
-const String CMD_TERMINATOR = "x";
+Component *components;
+
 CommandReader reader = CommandReader(&Serial1, CMD_TERMINATOR);
 
 void loop(void);
 void processCommand(String, String *, int);
 bool validateArguments(String, int, int); 
 
+void setupComponents() {
+  components = {
+    // DO NOT forget to update NUM_COMPONENTS
+    new StripComponent(STRIP_LED_COUNT, STRIP_DATA_PIN, STRIP_CLOCK_PIN)
+  };
+}
+
 extern "C" int main(void)
 {
 	Serial.begin(9600);
   Serial1.begin(9600);
+
+  setupComponents();
 
   while (1) {
     loop();
@@ -29,10 +41,16 @@ extern "C" int main(void)
 
 void loop() {
   reader.read(&processCommand);
-  stripComponent->update();
+  for (int i = 0; i < NUM_COMPONENTS; i++) {
+    components[i].update();
+  }
 }
 
 void processCommand(String cmd, String *args, int size) {
-  stripComponent->processCommand(cmd, args, size);
+  Serial.println("Command Received");
+  if (cmd == "command") {
+    int component = args[0].toInt();
+    components[component].processCommand(args[1], args + 2, size - 2);
+  }
 }
 
